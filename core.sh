afcc6466a76fa592525be2b1428a3dde9c03d1b3
pyseed <pyseed.dev@gmail.com>
2021-03-31 19:38:40 +0200

    [ADD] checkEnv

--- core.sh
+++ core.sh
@@ -9,7 +9,17 @@
 # checkArg name var
 checkArg () {
     if [ -z "$2" ]; then
-      echo "argument missing: $1"
+      echo ""
+      echo -e "\e[argument missing: $1\e[0m"
+      exit 1
+   fi
+}
+
+# check environment variable
+# checkEnv name var
+checkEnv () {
+    if [ -z "$2" ]; then
+      echo -e "\e[environment variable is not set: $1\e[0m"
       exit 1
    fi
 }
