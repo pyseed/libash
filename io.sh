0ce6ba5fdcea0b77d374788be93684ec99177b0d
vgreiner <vgreiner@anybox.fr>
2021-04-24 10:43:34 +0200

    [IMP] color

--- io.sh
+++ io.sh
@@ -5,6 +5,27 @@
 # unit test tools
 #
 
+_colors=(
+    [black]="$(echo -e '\e[30m')"
+    [red]="$(echo -e '\e[31m')"
+    [green]="$(echo -e '\e[32m')"
+    [yellow]="$(echo -e '\e[33m')"
+    [blue]="$(echo -e '\e[34m')"
+    [purple]="$(echo -e '\e[35m')"
+    [cyan]="$(echo -e '\e[36m')"
+    [light_gray]="$(echo -e '\e[37m')"
+    [gray]="$(echo -e '\e[90m')"
+    [light_red]="$(echo -e '\e[91m')"
+    [light_green]="$(echo -e '\e[92m')"
+    [light_yellow]="$(echo -e '\e[93m')"
+    [light_blue]="$(echo -e '\e[94m')"
+    [light_purple]="$(echo -e '\e[95m')"
+    [light_cyan]="$(echo -e '\e[96m')"
+    [white]="$(echo -e '\e[97m')"
+    [reset]="$(echo -e '\e[0m')"
+)
+
+
 # mark a pause
 # pause will not be applyied if NONINTERACTIVE=true
 pause () {
@@ -14,13 +35,13 @@ pause () {
 # display error in red
 # error ...args
 error () {
-    echo -e "\e[31mERROR\e[0m $*"
+    echo "${_colors[red]}[ERROR] ${*}${__colors[reset]}"
 }
 
 # display fatal error in red then exit
 # fatal ...args
 fatal () {
-    echo -e "\e[31mFATAL\e[0m $*"
+    echo "${_colors[red]}[FATAL] ${*}${__colors[reset]}"
     echo "aborted"
     exit 1
 }
@@ -28,13 +49,13 @@ fatal () {
 # display warning in orange
 # warning ...args
 warning () {
-    echo -e "\e[33mWARNING\e[0m $*"
+    echo "${_colors[yellow]}[WARNING] ${*}${__colors[reset]}"
 }
 
 # highlight success in green
 # success ...args
 success () {
-    echo -e "\e[32mERROR\e[0m $*"
+    echo "${_colors[green]}[SUCCESS] ${*}${__colors[reset]}"
 }
 
 # get timestamp (second precision)
